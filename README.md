ff81110943c210d260eaf525bcdc67d6f5fd8634
Mark Shust <mark@shust.com>
2015-11-21 00:12:47 -0500

    Updated setup script to only use composer to install. Make install mandatory. Removed github functionality to streamline code and make things simpler. Updated README to reference docker-compose repo for more info.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Versions
 
-- [`1.1.0`, `1.1`, `latest` (_Dockerfile_)](https://github.com/mageinferno/docker-magento2-setup/blob/master/Dockerfile)
+- [`2.0.0`, `2.0`, `latest` (_Dockerfile_)](https://github.com/mageinferno/docker-magento2-setup/blob/master/Dockerfile)
+- [`1.1.0`, `1.1`, (_Dockerfile_)](https://github.com/mageinferno/docker-magento2-setup/blob/1.1.0/Dockerfile)
 - [`1.0.1`, `1.0` (_Dockerfile_)](https://github.com/mageinferno/docker-magento2-setup/blob/1.0.1/Dockerfile)
 
 # Description
@@ -19,45 +20,8 @@ Once your docker-compose.yml file is all setup, you can execute setup by running
 
 `docker-compose run --rm setup`
 
-This will execute a one-off container that runs the setup script included in this Docker image.
-
-# Running Setup
-
-Before running Magento 2, you must download the source code, install composer dependencies, and execute the Magento installer script. Luckily, Mage Inferno makes this easy for you.
-
-The following environment variables can be set for setup:
-```
-      - M2SETUP_DB_HOST=db
-      - M2SETUP_DB_NAME=magento2
-      - M2SETUP_DB_USER=magento2
-      - M2SETUP_DB_PASSWORD=magento2
-      - M2SETUP_BASE_URL=http://mysite.docker/
-      - M2SETUP_ADMIN_FIRSTNAME=Admin
-      - M2SETUP_ADMIN_LASTNAME=User
-      - M2SETUP_ADMIN_EMAIL=dummy@gmail.com
-      - M2SETUP_ADMIN_USER=magento2
-      - M2SETUP_ADMIN_PASSWORD=magento2
-      - M2SETUP_GITHUB_BRANCH=merchant_beta
-      - M2SETUP_USE_SAMPLE_DATA=true
-      - M2SETUP_PULL_GITHUB=true
-      - M2SETUP_PULL_COMPOSER=true
-      - M2SETUP_PULL_NODE_MODULES=true
-      - M2SETUP_INSTALL=true
-```
-
-Our setup script uses these variables to determine how to setup your store. Everything is pretty self-explanatory, except the following:
-
-- `M2SETUP_PULL_GITHUB`: Setting this to `true` will download the latest stable Magento 2 code from the `merchant_beta` branch (or `M2SETUP_GITHUB_BRANCH` if defined), and check it out to your projects `src` directory. If you already have the code checked out from your host machine, set this to `false`.
-- `M2SETUP_PULL_COMPOSER`: Setting this to `true` will page through `composer.json` and install all composer dependencies to `src/vendor`. If you already have these installed, set this to `false`.
-- `M2SETUP_PULL_NODE_MODULES`: Setting this to `true` will page through `package.json` and install all node module dependencies to `src/node_modules`. If you already have these installed, set this to `false`.
-- `M2SETUP_INSTALL`: Setting this to `true` will install Magento 2 by the command line tool. If you already have Magento 2 installed, set this to `false`.
-
-To run setup, execute the following command from your project directory (`~/Sites/mysite`), which creates a one-off throw away container that sets up Magento 2 for you.
-
-`docker-compose run --rm setup`
-
-Note that setup will take between 30 and 40 minutes to complete. A vast majority of this time is from downloading Composer dependencies, and installing sample data (configurable products, specifically). Setting `M2SETUP_USE_SAMPLE_DATA` to false will expedite the install process by skipping the installation of sample data.
+This will execute a one-off container that runs the setup script included in this Docker image. Note that setup will take around 30 minutes to complete. A vast majority of this time is from downloading Composer dependencies, and installing sample data (configurable products, specifically). Setting `M2SETUP_USE_SAMPLE_DATA` to false will expedite the install process by skipping the installation of sample data.
 
 # Additional Info
 
-For additional info, please see the extended Readme at [https://github.com/mageinferno/magento2-docker-compose](https://github.com/mageinferno/magento2-docker-compose).
+For additional info, please see the extended docker-compose README at [https://github.com/mageinferno/magento2-docker-compose](https://github.com/mageinferno/magento2-docker-compose).
--- m2setup.sh
+++ m2setup.sh
@@ -1,78 +1,43 @@
 #!/bin/sh
 echo "Initializing setup..."
 
-if [ "$M2SETUP_GITHUB_BRANCH" = "" ]; then
-  M2SETUP_GITHUB_BRANCH=merchant_beta
-fi
+/usr/local/bin/composer self-update
+/usr/local/bin/composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition /src
 
-if [ "$M2SETUP_PULL_GITHUB" = true ]; then
-  echo "Pulling latest Magento 2 code from GitHub ($M2SETUP_GITHUB_BRANCH branch), this may take a few moments..."
-  git clone -b $M2SETUP_GITHUB_BRANCH https://github.com/magento/magento2.git /src
-fi
-
-if [ "$M2SETUP_PULL_COMPOSER" = true ]; then
-  echo "Installing composer dependencies..."
-  cd /src && composer install
-
-  if [ "$M2SETUP_USE_SAMPLE_DATA" = true ]; then
-    echo "Installing sample data..."
-    git clone -b $M2SETUP_GITHUB_BRANCH https://github.com/magento/magento2-sample-data.git /src-sample-data
-    php -f /src-sample-data/dev/tools/build-sample-data.php -- --ce-source=/src
-    find /src-sample-data -type d -exec chmod 770 {} \;
-    find /src-sample-data -type f -exec chmod 660 {} \;
-  fi
-fi
+chmod +x /src/bin/magento
 
 if [ "$M2SETUP_USE_SAMPLE_DATA" = true ]; then
+  echo "Installing composer dependencies..."
+  /src/bin/magento sampledata:deploy
+
+  echo "Ignore the above error (bug in Magento), fixing with 'composer update'..."
+  composer update
+
   M2SETUP_USE_SAMPLE_DATA_STRING="--use-sample-data"
 else
   M2SETUP_USE_SAMPLE_DATA_STRING=""
 fi
 
-if [ "$M2SETUP_INSTALL" = true ]; then
-  if [ -f /src/app/etc/config.php ] || [ -f /src/app/etc/env.php ]; then
-    echo "Already installed? Either app/etc/config.php or app/etc/env.php exist, please remove both files to continue setup."
-    exit
-  fi
-
-  if [ "$M2SETUP_PULL_GITHUB" = false ] && [ "$M2SETUP_PULL_COMPOSER" = false ]; then
-    # Wait a few for mysql container initialization...
-    /bin/sleep 10
-  fi
-
-  echo "Running Magento 2 setup script..."
-  /src/bin/magento setup:install \
-    --db-host=$M2SETUP_DB_HOST \
-    --db-name=$M2SETUP_DB_NAME \
-    --db-user=$M2SETUP_DB_USER \
-    --db-password=$M2SETUP_DB_PASSWORD \
-    --base-url=$M2SETUP_BASE_URL \
-    --admin-firstname=$M2SETUP_ADMIN_FIRSTNAME \
-    --admin-lastname=$M2SETUP_ADMIN_LASTNAME \
-    --admin-email=$M2SETUP_ADMIN_EMAIL \
-    --admin-user=$M2SETUP_ADMIN_USER \
-    --admin-password=$M2SETUP_ADMIN_PASSWORD \
-    $M2SETUP_USE_SAMPLE_DATA_STRING
-
-  echo "Reindexing all indexes..."
-  /src/bin/magento indexer:reindex
-
-  echo "Building static content..."
-  rm -rf /src/pub/static /src/var/cache /src/var/page_cache /src/var/view_preprocessed
-  /src/bin/magento setup:static-content:deploy
+if [ -f /src/app/etc/config.php ] || [ -f /src/app/etc/env.php ]; then
+  echo "Already installed? Either app/etc/config.php or app/etc/env.php exist, please remove both files to continue setup."
+  exit
 fi
 
-if [ "$M2SETUP_PULL_NODE_MODULES" = true ]; then
-  echo "Installing node modules..."
-  cp /src/package.json /root/
-  cd /root && npm install -q
-  echo "Node modules installed. Syncing back to /src/node_modules..."
-  rsync -rlzuIO --ignore-errors /root/node_modules/ /src/node_modules > /dev/null 2>&1
-  rm -rf /root/node_modules /root/package.json
+echo "Running Magento 2 setup script..."
+/src/bin/magento setup:install \
+  --db-host=$M2SETUP_DB_HOST \
+  --db-name=$M2SETUP_DB_NAME \
+  --db-user=$M2SETUP_DB_USER \
+  --db-password=$M2SETUP_DB_PASSWORD \
+  --base-url=$M2SETUP_BASE_URL \
+  --admin-firstname=$M2SETUP_ADMIN_FIRSTNAME \
+  --admin-lastname=$M2SETUP_ADMIN_LASTNAME \
+  --admin-email=$M2SETUP_ADMIN_EMAIL \
+  --admin-user=$M2SETUP_ADMIN_USER \
+  --admin-password=$M2SETUP_ADMIN_PASSWORD \
+  $M2SETUP_USE_SAMPLE_DATA_STRING
 
-  echo "Running initial grunt refresh..."
-  # Run grunt refresh twice; first fails, second completes (GitHub issue #1732)
-  cd /src && grunt refresh && grunt refresh
-fi
+echo "Reindexing all indexes..."
+/src/bin/magento indexer:reindex
 
 echo "The setup script has completed execution."
