f6d5e40e5afa474bd99270b36c8bd9d2fb2045f3
Mark Shust <mark@shust.com>
2015-11-23 00:05:05 -0500

    Added magento 2.0.0 archive with sample data

--- m2setup.sh
+++ m2setup.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
 echo "Initializing setup..."
 
-/usr/local/bin/composer self-update
-/usr/local/bin/composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition /src
+tar xzfo /usr/local/share/magento-ce-2.0.0-sd.tar.gz -C /src
+rm /usr/local/share/magento-ce-2.0.0-sd.tar.gz
 
 chmod +x /src/bin/magento
-
-if [ "$M2SETUP_USE_SAMPLE_DATA" = true ]; then
-  echo "Installing composer dependencies..."
-  /src/bin/magento sampledata:deploy
-
-  echo "Ignore the above error (bug in Magento), fixing with 'composer update'..."
-  composer update
-
-  M2SETUP_USE_SAMPLE_DATA_STRING="--use-sample-data"
-else
-  M2SETUP_USE_SAMPLE_DATA_STRING=""
-fi
 
 if [ -f /src/app/etc/config.php ] || [ -f /src/app/etc/env.php ]; then
   echo "Already installed? Either app/etc/config.php or app/etc/env.php exist, please remove both files to continue setup."
@@ -35,7 +23,7 @@ echo "Running Magento 2 setup script..."
   --admin-email=$M2SETUP_ADMIN_EMAIL \
   --admin-user=$M2SETUP_ADMIN_USER \
   --admin-password=$M2SETUP_ADMIN_PASSWORD \
-  $M2SETUP_USE_SAMPLE_DATA_STRING
+  --use-sample-data
 
 echo "Reindexing all indexes..."
 /src/bin/magento indexer:reindex
