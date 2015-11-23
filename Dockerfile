358c1f605a911fb6379272e625ca15399538b2bb
Mark Shust <mark@shust.com>
2015-11-23 17:58:21 -0500

    Fix issue gzip: stdin: not in gzip format #2

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM mageinferno/magento2-php:5.6-fpm
 MAINTAINER Mark Shust <mark.shust@mageinferno.com>
 
-COPY magento-ce-2.0.0-sd.tar.gz /usr/local/share/
 COPY m2setup.sh /usr/local/bin/
 
 CMD ["/usr/local/bin/m2setup.sh"]
--- m2setup.sh
+++ m2setup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 echo "Initializing setup..."
 
-tar xzfo /usr/local/share/magento-ce-2.0.0-sd.tar.gz -C /src
-rm /usr/local/share/magento-ce-2.0.0-sd.tar.gz
+curl -L https://github.com/mageinferno/docker-magento2-setup/raw/2.0.0-sd/magento-ce-2.0.0-sd.tar.gz | tar xzf - -o -C /src
 
 chmod +x /src/bin/magento
 
